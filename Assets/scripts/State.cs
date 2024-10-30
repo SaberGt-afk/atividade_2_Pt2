0a8dad4a4366896ba985ca0bdeda28386441b013
SaberGt-afk <gtsaber0000@gmail.com>
2024-10-30 18:07:53 +0000

    Chasing the Player

--- Assets/scripts/State.cs
+++ Assets/scripts/State.cs
@@ -7,13 +7,14 @@ public class State
 {
     public enum STATE
     {
-    IDLE, PATROL, PURSUE, ATTACK, SLEEP
+        IDLE, PATROL, PURSUE, ATTACK, SLEEP
     };
 
     public enum EVENT
     {
         ENTER, UPDATE, EXIT
     };
+
     public STATE name;
     protected EVENT stage;
     protected GameObject npc;
@@ -26,27 +27,95 @@ public class State
     float visAngle = 30.0f;
     float shootDist = 7.0f;
 
-        public State(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
-        {
-            npc = _npc;
-            agent = _agent;
-            anim = _anim;
-            stage = EVENT.ENTER;
-            player = _player;
-        }
+    public State(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+    {
+        npc = _npc;
+        agent = _agent;
+        anim = _anim;
+        stage = EVENT.ENTER;
+        player = _player;
+    }
 
-        public virtual void Enter() { stage = EVENT.UPDATE; }
-        public virtual void Update() { stage = EVENT.UPDATE; }
-        public virtual void Exit() { stage = EVENT.EXIT; }
-        public State Process()
+    public virtual void Enter() { stage = EVENT.UPDATE; }
+    public virtual void Update() { stage = EVENT.UPDATE; }
+    public virtual void Exit() { stage = EVENT.EXIT; }
+
+    public State Process()
+    {
+        if (stage == EVENT.ENTER) Enter();
+        if (stage == EVENT.UPDATE) Update();
+        if (stage == EVENT.EXIT)
         {
-            if (stage == EVENT.ENTER) Enter();
-            if (stage == EVENT.UPDATE) Update();
-            if (stage == EVENT.EXIT)
-            {
-                Exit();
-                return nextState;
-            }
-            return this;
-        } 
+            Exit();
+            return nextState;
+        }
+        return this;
+    } 
+}
+
+public class Idle : State
+{
+    public Idle(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+        : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.IDLE;
+    }
+
+    public override void Enter()
+    {
+        anim.SetTrigger("isIdle");
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        if (Random.Range(0, 100) < 10)
+        {
+            nextState = new Patrol(npc, agent, anim, player);
+            stage = EVENT.EXIT;
+        }
+    }
+
+    public override void Exit()
+    {
+        anim.ResetTrigger("isIdle");
+        base.Exit();
+    }
+}
+
+public class Patrol : State
+{
+    int currentIndex = -1;
+
+    public Patrol(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+        : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.PATROL;
+        agent.speed = 2;
+        agent.isStopped = false;
+    }
+
+    public override void Enter()
+    {
+        currentIndex = 0;
+        anim.SetTrigger("isWalking");
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        if (agent.remainingDistance < 1)
+        {
+            // Reinicia o índice quando atingir o final da lista de checkpoints
+            currentIndex = (currentIndex + 1) % GameEnvironment.Singleton.Checkpoints.Count;
+
+            agent.SetDestination(GameEnvironment.Singleton.Checkpoints[currentIndex].transform.position);
+        }
+    }
+
+    public override void Exit()
+    {
+        anim.ResetTrigger("isWalking");
+        base.Exit();
+    }
 }
